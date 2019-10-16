130a9b71a190d572854826d78246e7589d00e6bc
Zizo1414 <zizo@makeitsimple.co.uk>
2019-10-16 23:20:23 +0200

    Add some styles to main list

--- lib/expenses_list.dart
+++ lib/expenses_list.dart
@@ -28,40 +28,49 @@ class ExpensesList extends StatelessWidget {
       child: ListView.builder(
         itemCount: listItems.length,
         itemBuilder: (context, index) {
-          return (ListTile(
-            key: ValueKey(listItems[index].id),
-            leading: CircleAvatar(
-              // backgroundColor: Theme.of(context).primaryColor,
-              backgroundColor: colors[Random().nextInt(4)],
-              foregroundColor: Theme.of(context).accentColor,
-              radius: 30,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FittedBox(
-                  child: Text(listItems[index].amount),
+          return Dismissible(
+            background: Container(
+              // child: Align(
+              //   child: Icon(
+              //     Icons.delete,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              color: Colors.red,
+            ),
+            key: Key(listItems[index].id),
+            onDismissed: (_) {
+              removeItem(listItems[index].id);
+            },
+            child: (ListTile(
+              // key: ValueKey(listItems[index].id),
+              leading: CircleAvatar(
+                // backgroundColor: Theme.of(context).primaryColor,
+                backgroundColor: colors[Random().nextInt(4)],
+                foregroundColor: Theme.of(context).accentColor,
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FittedBox(
+                    child: Text(listItems[index].amount),
+                  ),
                 ),
               ),
-            ),
-            title: Text(
-              listItems[index].title,
-            ),
-            subtitle: Text(
-              '${listItems[index].category}\n${DateFormat.yMMMMd().format(listItems[index].date)}',
-            ),
-            trailing: Container(
-              width: MediaQuery.of(context).size.width * .05,
-              child: FlatButton(
-                padding: EdgeInsets.only(right: 0),
-                child: const Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-                onPressed: () {
-                  removeItem(listItems[index].id);
-                },
+              title: Text(
+                listItems[index].title,
               ),
-            ),
-          ));
+              subtitle: Text(
+                '${listItems[index].category}\n${DateFormat.yMMMMd().format(listItems[index].date)}',
+              ),
+              trailing: Container(
+                child: Icon(
+                  Icons.mood,
+                  size: 40,
+                  color: colors[Random().nextInt(4)],
+                ),
+              ),
+            )),
+          );
         },
       ),
     );
